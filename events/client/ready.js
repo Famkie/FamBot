e52077af752c96b2c749f76a549bb21b4a763df9
Famkie <famkie.id@gmail.com>
2025-05-19 23:13:09 +0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'ready',
-  once: true, // dijalankan sekali saja saat bot nyala
+  once: true,
   execute(client) {
     console.log(`✅ Bot ${client.user.tag} sudah online!`);
     client.user.setActivity('/help | KieBot', { type: 'PLAYING' }); // Status bot
