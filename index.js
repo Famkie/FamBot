f41f6f3bd1b1ae4edff33b5e2364fed7e963ab03
Famkie <famkie.id@gmail.com>
2025-05-20 17:34:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import express from 'express';
 import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
@@ -32,25 +32,33 @@ const client = new Client({
 client.commands = new Collection();
 client.aliases = new Collection();
 
-// Load semua commands dari folder commands
-await loadCommands(client, path.join(__dirname, 'commands'));
+async function main() {
+  // Load semua commands dari folder commands
+  await loadCommands(client, path.join(__dirname, 'commands'));
 
-// Load semua events dari folder events
-const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(f => f.endsWith('.js'));
-for (const file of eventFiles) {
-  const { default: event } = await import(path.join(__dirname, 'events', file));
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
+  // Load semua events dari folder events (async version)
+  const eventsPath = path.join(__dirname, 'events');
+  const eventFiles = (await fs.readdir(eventsPath)).filter(f => f.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const eventPath = path.join(eventsPath, file);
+    const { default: event } = await import(`file://${eventPath}`);
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client));
+    }
   }
+
+  // Event interactionCreate untuk slash commands
+  client.on('interactionCreate', interaction => handleInteraction(interaction, client));
+
+  // Event messageCreate untuk prefix commands
+  client.on('messageCreate', message => handleMessage(message, client, config.prefix || '!'));
+
+  // Login ke Discord
+  await client.login(config.token);
 }
 
-// Event interactionCreate untuk slash commands
-client.on('interactionCreate', interaction => handleInteraction(interaction, client));
-
-// Event messageCreate untuk prefix commands
-client.on('messageCreate', message => handleMessage(message, client, config.prefix || '!'));
-
-// Login ke Discord
-client.login(config.token);
+main().catch(console.error);
