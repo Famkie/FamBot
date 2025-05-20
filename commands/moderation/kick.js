b85f501e8929f8d7714e6c38b598ceb12ca8461d
Famkie <famkie.id@gmail.com>
2025-05-20 19:38:28 +0700

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,20 +1,24 @@
-module.exports = {
-  name: 'kick',
-  description: 'Kick seorang member dari server.',
-  execute(message, args) {
-    if (!message.member.permissions.has('KickMembers'))
-      return message.reply("Kamu tidak punya izin untuk kick.");
+export const name = 'kick';
+export const description = 'Kick seorang member dari server.';
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply("Mention user yang ingin kamu kick.");
-
-    if (!member.kickable) return message.reply("Aku tidak bisa kick user itu!");
-
-    member.kick().then(() => {
-      message.channel.send(`${member.user.tag} telah di-kick.`);
-    }).catch(err => {
-      message.channel.send("Gagal kick user.");
-      console.error(err);
-    });
+export function execute(message, args) {
+  if (!message.member.permissions.has('KickMembers')) {
+    return message.reply("Kamu tidak punya izin untuk kick.");
   }
-};
+
+  const member = message.mentions.members.first();
+  if (!member) {
+    return message.reply("Mention user yang ingin kamu kick.");
+  }
+
+  if (!member.kickable) {
+    return message.reply("Aku tidak bisa kick user itu!");
+  }
+
+  member.kick().then(() => {
+    message.channel.send(`${member.user.tag} telah di-kick.`);
+  }).catch(err => {
+    message.channel.send("Gagal kick user.");
+    console.error(err);
+  });
+}
