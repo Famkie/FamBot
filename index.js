de5d2752e083b832b87d425f75bf796ec6c3b436
Famkie <famkie.id@gmail.com>
2025-05-20 14:03:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
-require('dotenv').config();
-const config = require('./config/config.js');
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import config from './config/config.js';
+import loadCommands from './handlers/commandHandler.js';
 
-const express = require('express');
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Keepalive
 const app = express();
-app.get('/', (req, res) => res.send('Bot is running!'));
+app.get('/', (_, res) => res.send('Bot is running!'));
 app.listen(3000, () => console.log('Keepalive aktif di port 3000'));
 
+// Bot client setup
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -22,14 +32,18 @@ const client = new Client({
 client.commands = new Collection();
 client.aliases = new Collection();
 
-require('./handlers/commandHandler')(client);
+// Load command handler
+await loadCommands(client);
 
-const commandFolders = fs.readdirSync('./commands');
+// Load commands from ./commands
+const commandFolders = fs.readdirSync(path.join(__dirname, 'commands'));
 for (const folder of commandFolders) {
-  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  const commandFiles = fs
+    .readdirSync(path.join(__dirname, 'commands', folder))
+    .filter(file => file.endsWith('.js'));
 
   for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
+    const { default: command } = await import(`./commands/${folder}/${file}`);
     if (!command.name) continue;
 
     client.commands.set(command.name, command);
@@ -39,10 +53,11 @@ for (const folder of commandFolders) {
   }
 }
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+// Load events from ./events
+const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
+  const { default: event } = await import(`./events/${file}`);
   if (event.once) {
     client.once(event.name, (...args) => event.execute(...args, client));
   } else {
