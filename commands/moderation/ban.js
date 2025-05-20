91faba2d19d42e041ff8055b0ae58e4e0341c1b1
Famkie <famkie.id@gmail.com>
2025-05-20 19:36:30 +0700

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,20 +1,21 @@
-module.exports = {
-  name: 'ban',
-  description: 'Ban seorang member dari server.',
-  execute(message, args) {
-    if (!message.member.permissions.has('BanMembers'))
-      return message.reply("Kamu tidak punya izin untuk ban.");
+export const name = 'ban';
+export const description = 'Ban seorang member dari server.';
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply("Mention user yang ingin kamu ban.");
+export function execute(message, args) {
+  if (!message.member.permissions.has('BanMembers')) {
+    return message.reply("Kamu tidak punya izin untuk ban.");
+  }
 
-    if (!member.bannable) return message.reply("Aku tidak bisa ban user itu!");
+  const member = message.mentions.members.first();
+  if (!member) return message.reply("Mention user yang ingin kamu ban.");
+  if (!member.bannable) return message.reply("Aku tidak bisa ban user itu!");
 
-    member.ban().then(() => {
+  member.ban()
+    .then(() => {
       message.channel.send(`${member.user.tag} telah di-ban.`);
-    }).catch(err => {
+    })
+    .catch(err => {
       message.channel.send("Gagal ban user.");
       console.error(err);
     });
-  }
-};
+}
