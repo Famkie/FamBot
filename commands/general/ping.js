f81adc11b434f3763b6b0b8ff7f01131e04ffa1b
Famkie <famkie.id@gmail.com>
2025-05-20 17:57:01 +0700

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,21 +1,8 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-
-export default {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
-
-  // Slash command
-  async execute(interaction) {
-    if (interaction.isCommand()) {
-      await interaction.reply('Pong!');
-    }
-  },
-
-  // Prefix command
-  async execute(message, client, args) {
-    if (message.content.startsWith('!ping')) {
-      await message.channel.send('Pong!');
-    }
+export const command = {
+  name: 'ping',
+  description: 'Ping command',
+  aliases: ['p'],
+  async execute(message, args, client) {
+    await message.reply('Pong!');
   }
 };
