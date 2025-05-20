f396cdad5ac24a4d5a0d4431c2835c9aa0506aa1
Famkie <famkie.id@gmail.com>
2025-05-20 17:30:26 +0700

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = (client) => {
+export async function registerEvents(client) {
+  // Dapatkan direktori current file (ESM tidak punya __dirname)
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
   const eventsPath = path.join(__dirname, '..', 'events');
-  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+  const eventFiles = (await fs.readdir(eventsPath)).filter(file => file.endsWith('.js'));
 
   for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
+    // import dinamis ESM
+    const event = await import(`file://${filePath}`);
 
-    if (event.once) {
-      client.once(event.name, (...args) => event.execute(...args, client));
+    if (event.default.once) {
+      client.once(event.default.name, (...args) => event.default.execute(...args, client));
     } else {
-      client.on(event.name, (...args) => event.execute(...args, client));
+      client.on(event.default.name, (...args) => event.default.execute(...args, client));
     }
   }
 
-  console.log(`[EVENTS] ${eventFiles.length} event loaded.`);
-};
+  console.log(`[EVENTS] ${eventFiles.length} event(s) loaded.`);
+}
