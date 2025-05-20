0d55cd870ec47826f7433bc01f28514dc3f79dbc
Famkie <famkie.id@gmail.com>
2025-05-20 13:27:07 +0700

    Update linfo.js

--- commands/lotto/linfo.js
+++ commands/lotto/linfo.js
@@ -1,29 +1,62 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const lottoStore = require('../../utils/lottoStore');
-const fs = require('fs');
-const path = require('path');
+import { SlashCommandBuilder } from 'discord.js';
+import { lottoManager } from '../../utils/lottoManager.js';
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('linfo')
-    .setDescription('Tampilkan informasi lotto yang sedang berjalan'),
+export const data = new SlashCommandBuilder()
+  .setName('info')
+  .setDescription('Show the current active lotto info.');
 
-  async execute(interaction) {
-    const lotto = lottoStore.activeLotto;
-    if (!lotto) return interaction.reply('Tidak ada undian yang sedang berjalan.');
+export const command = {
+  name: 'info',
+  aliases: ['info'],
+  description: 'Show info about the current active lotto.',
 
-    const embed = new EmbedBuilder()
-      .setTitle(`Lotto Berjalan - ${lotto.prize}`)
-      .addFields(
-        { name: 'Durasi', value: `${lotto.duration / 1000}s`, inline: true },
-        { name: 'Jumlah Pemenang', value: `${lotto.winners}`, inline: true },
-        { name: 'Jumlah Peserta', value: `${lotto.entries.length}`, inline: true },
-      )
-      .setDescription(
-        lotto.entries.map((e, i) => `**${i + 1}.** ${e.username} [${e.tornId}]`).join('\n') || 'Belum ada peserta'
-      )
-      .setColor('Blue');
+  async execute(message) {
+    const lotto = lottoManager.getLotto(message.guild.id);
 
-    interaction.reply({ embeds: [embed] });
-  }
+    if (!lotto) {
+      return message.reply('No active lotto in this server.');
+    }
+
+    const embed = {
+      color: 0x00bfff,
+      title: 'Active Lotto Info',
+      fields: [
+        { name: 'Host', value: `<@${lotto.host}>`, inline: true },
+        { name: 'Prize', value: lotto.prize.toString(), inline: true },
+        { name: 'Base', value: lotto.base.toString(), inline: true },
+        { name: 'Added', value: lotto.added.toString(), inline: true },
+        { name: 'Karma Mode', value: lotto.karma ? 'Enabled' : 'Disabled', inline: true },
+        { name: 'Faction Mode', value: lotto.faction ? 'Enabled' : 'Disabled', inline: true },
+        { name: 'Participants', value: `${lotto.entries.length} users`, inline: true },
+        { name: 'Created At', value: `<t:${Math.floor(lotto.createdAt / 1000)}:F>`, inline: false },
+      ],
+    };
+
+    return message.channel.send({ embeds: [embed] });
+  },
+
+  async slash(interaction) {
+    const lotto = lottoManager.getLotto(interaction.guild.id);
+
+    if (!lotto) {
+      return interaction.reply({ content: 'No active lotto in this server.', ephemeral: true });
+    }
+
+    const embed = {
+      color: 0x00bfff,
+      title: 'Active Lotto Info',
+      fields: [
+        { name: 'Host', value: `<@${lotto.host}>`, inline: true },
+        { name: 'Prize', value: lotto.prize.toString(), inline: true },
+        { name: 'Base', value: lotto.base.toString(), inline: true },
+        { name: 'Added', value: lotto.added.toString(), inline: true },
+        { name: 'Karma Mode', value: lotto.karma ? 'Enabled' : 'Disabled', inline: true },
+        { name: 'Faction Mode', value: lotto.faction ? 'Enabled' : 'Disabled', inline: true },
+        { name: 'Participants', value: `${lotto.entries.length} users`, inline: true },
+        { name: 'Created At', value: `<t:${Math.floor(lotto.createdAt / 1000)}:F>`, inline: false },
+      ],
+    };
+
+    return interaction.reply({ embeds: [embed] });
+  },
 };
