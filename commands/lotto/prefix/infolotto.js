022f94ab88f1d7aa61ba22a82768a97866e0d935
Famkie <famkie.id@gmail.com>
2025-05-20 16:13:04 +0700

    Create infolotto.js

---
+++ commands/lotto/prefix/infolotto.js
@@ -0,0 +1,26 @@
+import { lottoManager } from '../../utils/lottoManager.js';
+
+export async function executePrefixCommand(message) {
+  const lotto = lottoManager.getLotto(message.guild.id);
+
+  if (!lotto) {
+    return message.reply('No active lotto in this server.');
+  }
+
+  const embed = {
+    color: 0x00bfff,
+    title: 'Active Lotto Info',
+    fields: [
+      { name: 'Host', value: `<@${lotto.host}>`, inline: true },
+      { name: 'Prize', value: lotto.prize.toString(), inline: true },
+      { name: 'Base', value: lotto.base.toString(), inline: true },
+      { name: 'Added', value: lotto.added.toString(), inline: true },
+      { name: 'Karma Mode', value: lotto.karma ? 'Enabled' : 'Disabled', inline: true },
+      { name: 'Faction Mode', value: lotto.faction ? 'Enabled' : 'Disabled', inline: true },
+      { name: 'Participants', value: `${lotto.entries.length} users`, inline: true },
+      { name: 'Created At', value: `<t:${Math.floor(lotto.createdAt / 1000)}:F>`, inline: false },
+    ],
+  };
+
+  return message.channel.send({ embeds: [embed] });
+}
