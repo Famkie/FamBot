f83f5299095ed6fe98e5b5e75fa9716602ef6380
Famkie <famkie.id@gmail.com>
2025-05-20 19:43:36 +0700

    Update tclink.js

--- commands/torn/tclink.js
+++ commands/torn/tclink.js
@@ -1,32 +1,30 @@
-const { SlashCommandBuilder } = require('discord.js');
-const fetchTornData = require('../../utils/fetchTorn');
-const { setTornUser } = require('../../utils/tornUsers');
+import { SlashCommandBuilder } from 'discord.js';
+import fetchTornData from '../../utils/fetchTorn.js';
+import { setTornUser } from '../../utils/tornUsers.js';
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('tclink')
-    .setDescription('Verifikasi akun Torn City kamu')
-    .addStringOption(option =>
-      option.setName('apikey')
-        .setDescription('API key Torn City kamu')
-        .setRequired(true)
-    ),
+export const data = new SlashCommandBuilder()
+  .setName('tclink')
+  .setDescription('Verifikasi akun Torn City kamu')
+  .addStringOption(option =>
+    option.setName('apikey')
+      .setDescription('API key Torn City kamu')
+      .setRequired(true)
+  );
 
-  async execute(interaction) {
-    await interaction.deferReply({ ephemeral: true });
+export async function execute(interaction) {
+  await interaction.deferReply({ ephemeral: true });
 
-    const apiKey = interaction.options.getString('apikey');
+  const apiKey = interaction.options.getString('apikey');
 
-    const data = await fetchTornData('user', 'basic,profile', apiKey);
+  const data = await fetchTornData('user', 'basic,profile', apiKey);
 
-    if (data.error) {
-      return interaction.editReply(`Gagal verifikasi: ${data.error}`);
-    }
-
-    await setTornUser(interaction.user.id, apiKey);
-
-    return interaction.editReply(
-      `Berhasil terverifikasi sebagai **${data.name}** [${data.player_id}]`
-    );
+  if (data.error) {
+    return interaction.editReply(`Gagal verifikasi: ${data.error}`);
   }
-};
+
+  await setTornUser(interaction.user.id, apiKey);
+
+  return interaction.editReply(
+    `Berhasil terverifikasi sebagai **${data.name}** [${data.player_id}]`
+  );
+}
