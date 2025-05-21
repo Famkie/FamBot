1de3b6e2a12f6861399598c4bf48dc1c7c089f05
Famkie <famkie.id@gmail.com>
2025-05-21 12:56:53 +0700

    Update discord.js

--- helper_functions/discord.js
+++ helper_functions/discord.js
@@ -1,25 +1,28 @@
-import { General_functions } from "./general.js"
+// helper_functions/discord.js
+import { General_functions } from './general.js';
 
-async function rename_user(user_id, guild_id, name) {
-	let client = General_functions.getClient()
-	let guild = client.guilds.cache.get(guild_id)
-	if (guild === undefined) {
-		return { "error": "Could not find guild" }
-	}
-	let member = await guild.members.fetch(user_id)
-	if (member === undefined) {
-		return { "error": "Could not find member" }
-	}
-	try {
-		let x = await member.setNickname(name)
-		return { "done": true }
-	} catch (error) {
-		return { "error": error }
-	}
+export async function rename_user(user_id, guild_id, name) {
+  const client = General_functions.getClient();
+  const guild = client.guilds.cache.get(guild_id);
+
+  if (!guild) {
+    return { error: 'Could not find guild' };
+  }
+
+  const member = await guild.members.fetch(user_id).catch(() => null);
+
+  if (!member) {
+    return { error: 'Could not find member' };
+  }
+
+  try {
+    await member.setNickname(name);
+    return { done: true };
+  } catch (error) {
+    return { error };
+  }
 }
 
-const Discord_functions = {
-	rename_user: rename_user,
-}
-
-export { Discord_functions }
+export const Discord_functions = {
+  rename_user
+};
