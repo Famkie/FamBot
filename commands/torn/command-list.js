6ccf1e711a8e43c78dbb14f3daa3cbb613082680
Famkie <famkie.id@gmail.com>
2025-05-21 09:33:54 +0700

    Update command-list.js

--- commands/torn/command-list.js
+++ commands/torn/command-list.js
@@ -1,29 +1,31 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } from 'discord.js';
-import {Alerts_general} from '../alerts/general.js'
-import {Embed_functions} from "../helper_functions/embeds.js"
+import { MessageEmbed } from 'discord.js';
+import { Alerts_general } from '../alerts/general.js';
+import { Embed_functions } from '../helper_functions/embeds.js';
 
-let data = new SlashCommandBuilder()
-		.setName('command-list')
-		.setDescription('Get list of commands of this bot.')
-		
-async function execute(interaction) {
-	let the_list = ""
-	for (const [key, value] of interaction.client.commands) {
-		the_list += value.data.name + "\n"
-	}
+export const data = new SlashCommandBuilder()
+  .setName('command-list')
+  .setDescription('Get list of commands of this bot.');
 
-	const embed = new MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle("Command list")
-		.addFields( [{name:"Commands", value:the_list, inline:true}] )
-		.setTimestamp()
-		.setFooter('Page 1/1', '')
+export async function execute(interaction) {
+  let the_list = '';
+  for (const [key, value] of interaction.client.commands) {
+    the_list += `${value.data.name}\n`;
+  }
 
-	let to_reply = await Embed_functions.check_reply( {embeds: [embed]}, interaction, 25, 25 )
-	await interaction.reply( to_reply );
+  const embed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Command list')
+    .addFields([{ name: 'Commands', value: the_list, inline: true }])
+    .setTimestamp()
+    .setFooter({ text: 'Page 1/1' });
+
+  const to_reply = await Embed_functions.check_reply(
+    { embeds: [embed] },
+    interaction,
+    25,
+    25
+  );
+
+  await interaction.reply(to_reply);
 }
-
-
-export { data }
-export { execute }
