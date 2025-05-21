c1bab197c24f683f823df94e1987796eb60666eb
Famkie <famkie.id@gmail.com>
2025-05-21 10:34:06 +0700

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,8 +1,12 @@
-export const command = {
-  name: 'ping',
-  description: 'Ping command',
-  aliases: ['p'],
-  async execute(message, args, client) {
-    await message.reply('Pong!');
-  }
-};
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+let data = new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong')
+	
+async function execute(interaction) {
+	await interaction.reply(`🏓Latency is ${Date.now() - interaction.createdTimestamp}ms.`);
+}
+
+export { data }
+export { execute }
