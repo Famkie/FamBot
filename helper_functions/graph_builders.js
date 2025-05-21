f75655e14a46067393e8e2e1a7205cf5c0582392
Famkie <famkie.id@gmail.com>
2025-05-21 13:05:11 +0700

    Update graph_builders.js

--- helper_functions/graph_builders.js
+++ helper_functions/graph_builders.js
@@ -1,104 +1,90 @@
 import QuickChart from "quickchart-js";
-import fs from "fs";
-import luxon from "luxon";
+import { DateTime } from "luxon";
 
 async function timeline_graph(
   data = {},
   title = "",
   x_label = "",
-  y_label = ""
+  y_label = "",
+  datasetLabel = "Data"
 ) {
   let dataset = {
-    label: "",
+    label: datasetLabel,
     backgroundColor: "rgba(75, 192, 192, 0.5)",
     borderColor: "rgb(75, 192, 192)",
     fill: false,
     data: []
   };
+
   for (let key of Object.keys(data)) {
-    dataset.data.push({ x: parseInt(key), y: data[key] });
+    // Asumsi key adalah Unix timestamp detik, konversi ke ISO string
+    let dateStr = DateTime.fromSeconds(parseInt(key)).toISO();
+    dataset.data.push({ x: dateStr, y: data[key] });
   }
 
   let options = {
-    title: {
-      display: true,
-      text: title,
-      fontColor: "rgb(143, 143, 143)",
-      fontSize: 40
-    },
-    legend: {
-      display: false
+    plugins: {
+      title: {
+        display: !!title,
+        text: title,
+        color: "rgb(143, 143, 143)",
+        font: { size: 40 }
+      },
+      legend: {
+        display: false
+      }
     },
     scales: {
-      xAxes: [
-        {
-          type: "time",
-          scaleLabel: {
-            display: true,
-            labelString: x_label,
-            fontColor: "rgb(143, 143, 143)",
-            fontSize: 30
-          },
-          ticks: {
-            fontColor: "rgb(143, 143, 143)",
-            fontSize: 25
-          },
-          gridLines: {
-            color: "rgb(143, 143, 143)",
-            zeroLineColor: "rgb(143, 143, 143)"
-          }
+      x: {
+        type: "time",
+        title: {
+          display: !!x_label,
+          text: x_label,
+          color: "rgb(143, 143, 143)",
+          font: { size: 30 }
+        },
+        ticks: {
+          color: "rgb(143, 143, 143)",
+          font: { size: 25 }
+        },
+        grid: {
+          color: "rgb(143, 143, 143)"
         }
-      ],
-      yAxes: [
-        {
-          scaleLabel: {
-            display: true,
-            labelString: y_label,
-            fontColor: "rgb(143, 143, 143)",
-            fontSize: 30
-          },
-          ticks: {
-            fontColor: "rgb(143, 143, 143)",
-            fontSize: 25
-          },
-          gridLines: {
-            color: "rgb(143, 143, 143)",
-            zeroLineColor: "rgb(143, 143, 143)"
-          }
+      },
+      y: {
+        title: {
+          display: !!y_label,
+          text: y_label,
+          color: "rgb(143, 143, 143)",
+          font: { size: 30 }
+        },
+        ticks: {
+          color: "rgb(143, 143, 143)",
+          font: { size: 25 }
+        },
+        grid: {
+          color: "rgb(143, 143, 143)"
         }
-      ]
+      }
     }
   };
-
-  if (title === "") {
-    options.title.display = false;
-  }
-  if (x_label === "") {
-    options.scales.xAxes[0].scaleLabel.display = false;
-  }
-  if (y_label === "") {
-    options.scales.yAxes[0].scaleLabel.display = false;
-  }
 
   const qc = new QuickChart();
   qc.setConfig({
     type: "line",
     data: {
-      labels: [],
       datasets: [dataset]
     },
     options: options
   });
-  qc.setWidth(1500)
-    .setHeight(800)
-    .setBackgroundColor("transparent");
+  qc.setWidth(1500).setHeight(800).setBackgroundColor("transparent");
 
   const image = await qc.toBinary();
   return image;
 }
 
 const Graphs_builders = {
-  timeline_graph: timeline_graph
+  timeline_graph
 };
 
 export { Graphs_builders };
